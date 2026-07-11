28b0e0632dc51b9315d70f0fdd4db3912e361163
SaraSaldivar <sarasaldivar472@gmail.com>
2026-07-10 21:18:58 -0600

    nomás un import q no servía de nada

--- src/academic/dto/update-schedule.dto.ts
+++ src/academic/dto/update-schedule.dto.ts
@@ -1,7 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateScheduleDto } from './create-schedule.dto';
 import { IsBoolean, IsOptional } from 'class-validator';
-import { TruthyTypesOf } from 'rxjs';
 
 
 export class UpdateScheduleDto extends PartialType(CreateScheduleDto) {
