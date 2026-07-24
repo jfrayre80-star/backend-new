1ebbff5e4609ae5068a7ff09f3f4661040587a70
SaraSaldivar <sarasaldivar472@gmail.com>
2026-07-23 23:08:41 -0600

    por fin pude hacer funcionar unos imports jsjsj

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { StartAttendanceDto } from "./dto/start-attendance.dto";
-import { AttendanceService } from "./attendance.service";
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AttendanceService } from './attendance.service';
+import { StartAttendanceDto, ScanQrDto } from './dto/start-attendance.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
-
-@Controller("attendance")
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Controller('attendance')
 export class AttendanceController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
-  @Post("start")
+  @Roles('teacher', 'admin')
+  @Post('start')
   start(@Body() dto: StartAttendanceDto) {
     return this.attendanceService.start(dto);
+  }
+
+  @Roles('student')
+  @Post('scan')
+  scanQr(@Req() req: any, @Body() dto: ScanQrDto) {
+    return this.attendanceService.scanQr(req.user.id, dto);
   }
 }
